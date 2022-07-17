981ab029b5543ce42e4e71279923bac314910577
Jorge de Jesus Perez Lopez <jodeperezlo@ittepic.edu.mx>
2022-07-17 14:58:20 -0600

    [Add] Changes until class number 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -10,6 +10,7 @@ export class AppComponent {
 	inputName = '';
 	name = 'Jorge';
 	age = 23;
+	color = 'rojo';
 	img =
 		'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Platzi.jpg/800px-Platzi.jpg';
 	btnDisabled = true;
@@ -28,6 +29,40 @@ export class AppComponent {
 		'Ricardo',
 		'Raul',
 		'Sergio',
+	];
+
+	products: Product[] = [
+		{
+			name: 'El mejor juguete',
+			price: 565,
+			image: './assets/images/toy.jpg',
+			category: 'all',
+		},
+		{
+			name: 'Bicicleta casi nueva',
+			price: 356,
+			image: './assets/images/bike.jpg',
+		},
+		{
+			name: 'Colleción de albumnes',
+			price: 34,
+			image: './assets/images/album.jpg',
+		},
+		{
+			name: 'Mis libros',
+			price: 23,
+			image: './assets/images/books.jpg',
+		},
+		{
+			name: 'Casa para perro',
+			price: 34,
+			image: './assets/images/house.jpg',
+		},
+		{
+			name: 'Gafas',
+			price: 3434,
+			image: './assets/images/glasses.jpg',
+		},
 	];
 
 	btnToggle() {
